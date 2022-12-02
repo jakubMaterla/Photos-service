1274175dd652e9e5acf42f1490e32e4eb989f639
Jakub <jakub@materla.com>
2022-12-02 19:07:14 +0100

    Autoryzacja użytkownika, dodanie formularza logowania, date fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('admin@localhost.local');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'strefakursow'));
+
+        $manager->persist($user);
+        $manager->flush();
+
+        $manager->flush();
+    }
+}
