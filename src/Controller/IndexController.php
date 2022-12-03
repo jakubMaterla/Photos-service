cfd1203b32b6c2c5133ebf2b54b850a1c2e227a6
Jakub <jakub@materla.com>
2022-12-03 21:46:30 +0100

    Dodanie formulrza do wysyłania zdjęć i jego obsługa w kontrolerze, wgranie zdjęć do folderu, obsługa komunikatów-Flashes

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,17 +2,62 @@
 
 namespace App\Controller;
 
+use App\Entity\Photo;
+use App\Form\UploadPhotoType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('index/index.html.twig', [
+        $form = $this->createForm(UploadPhotoType::class);
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+
+            if($this->getUser())
+            {
+                /** @var UploadedFile $pictureFileName */
+                $pictureFileName = $form->get('filename')->getData();
+                if ($pictureFileName) {
+                    try {
+                        $originalFileName = pathinfo($pictureFileName->getClientOriginalName(), PATHINFO_FILENAME);
+                        $safeFileName = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFileName);
+                        $newFileName = $safeFileName.'-'.uniqid().'.'.$pictureFileName->guessExtension();
+                        $pictureFileName->move('images/hosting', $newFileName);
+
+                        $entityPhotos = new Photo();
+                        $entityPhotos->setFilename($newFileName);
+                        $entityPhotos->setIsPublic($form->get('is_public')->getData());
+                        $entityPhotos->setUploadedAt(new \DateTimeImmutable());
+                        $entityPhotos->setUser($this->getUser());
+
+                        $em->persist($entityPhotos);
+                        $em->flush();
+
+                        $this->addFlash('success', 'Dodano zdjęcie!');
+                    } catch (\Exception $e)
+                    {
+                        $this->addFlash('error', 'Wystąpił nieoczekiwany bład!');
+                    }
+
+                }
+
+            }
+
+        }
+
+        return $this->render('index/index.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
