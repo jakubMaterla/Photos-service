ecdc67d05630bf0f1603362479579a35dd4e7357
Jakub <jakub@materla.com>
2022-12-12 20:32:31 +0100

    Utworzenie widoku dla Moje zdjęcia, utworzyony serwis dla ustawiania prywtne/publiczne

--- src/Controller/MyController.php
+++ src/Controller/MyController.php
@@ -3,92 +3,65 @@
 namespace App\Controller;
 
 use App\Entity\Photo;
+use App\Service\PhotoVisibilityService;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted("ROLE_USER")]
 class MyController extends AbstractController
 {
 
-    public function index()
+    #[Route('/my/photos', name: 'my_photos')]
+    public function index(EntityManagerInterface $em)
     {
+        $myPhotos = $em->getRepository(Photo::class)->findBy(['user' => $this->getUser()]);
 
+        return $this->render('my/index.html.twig', [
+            'myPhotos' => $myPhotos
+        ]);
     }
 
-    #[Route('/my/photos/set_private/{id}', name: 'my_photos_set_as_private')]
-    public function myPhotosSetAsPrivate(int $id, EntityManagerInterface $em)
+    #[Route('/my/photos/set_visibility/{id}/{visibility}', name: 'my_photos_set_visibility')]
+    public function myPhotosChangeVisibility(PhotoVisibilityService $photoVisibilityService, int $id, bool $visibility)
     {
-        $myPhoto = $em->getRepository(Photo::class)->find($id);
+        $messages = [
+            '1' => 'publiczne',
+            '0' => 'prywatne'
+        ];
 
-        if ($this->getUser() == $myPhoto->getUser())
-        {
-            try {
-                $myPhoto->setIsPublic(0);
-                $em->persist($myPhoto);
-                $em->flush();
-                $this->addFlash('success', 'Ustawiono jako prywatne.');
-
-            } catch (\Exception $e)
-            {
-                $this->addFlash('error', 'Wystąpił problem przy ustawianiu jako prywatne.');
-            }
-        }
+        if ($photoVisibilityService->makeVisibility($id, $visibility))
+            $this->addFlash('success', 'Ustawiono jako '.$messages[$visibility].'.');
         else
-        {
-            $this->addFlash('error', 'Nie jesteś włąścicielem tego zdjęcia.');
-        }
+            $this->addFlash('error', 'Wystąpił problem przy ustawianiu jako '.$messages[$visibility].'.');
 
-        return $this->redirectToRoute('latest_photos');
+        return $this->redirectToRoute('my_photos');
     }
 
-    #[Route('/my/photos/set_public/{id}', name: 'my_photos_set_as_public')]
-    public function myPhotosSetAsPublic(int $id, EntityManagerInterface $em)
-    {
-        $myPhoto = $em->getRepository(Photo::class)->find($id);
-
-        if ($this->getUser() == $myPhoto->getUser())
-        {
-            try {
-                $myPhoto->setIsPublic(1);
-                $em->persist($myPhoto);
-                $em->flush();
-                $this->addFlash('success', 'Ustawiono jako publiczne.');
-
-            } catch (\Exception $e)
-            {
-                $this->addFlash('error', 'Wystąpił problem przy ustawianiu jako publiczne.');
-            }
-        }
-        else
-        {
-            $this->addFlash('error', 'Nie jesteś włąścicielem tego zdjęcia.');
-        }
-
-        return $this->redirectToRoute('latest_photos');
-    }
     #[Route('/my/photos/remove/{id}', name: 'my_photos_remove')]
     public function myPhotoRemove(int $id, EntityManagerInterface $em)
     {
         $myPhoto = $em->getRepository(Photo::class)->find($id);
 
-        if ($this->getUser() == $myPhoto->getUser()) {
-            $fileManager = new Filesystem();
-            $fileManager->remove('images/hosting/'.$myPhoto->getFilename());
 
-            if ($fileManager->exists('images/hosting/'.$myPhoto->getFilename()))
-            {
-                $this->addFlash('error', 'Nie udało się usunąć zdjęcia');
-            }
-            else
-            {
-                $em->remove($myPhoto);
-                $em->flush();
-                $this->addFlash('success', 'Usunięto zdjęcie');
-            }
+        $fileManager = new Filesystem();
+        $fileManager->remove('images/hosting/'.$myPhoto->getFilename());
+
+        if ($fileManager->exists('images/hosting/'.$myPhoto->getFilename()))
+        {
+            $this->addFlash('error', 'Nie udało się usunąć zdjęcia');
         }
-        return $this->redirectToRoute('latest_photos');
+        else
+        {
+            $em->remove($myPhoto);
+            $em->flush();
+            $this->addFlash('success', 'Usunięto zdjęcie');
+        }
+
+        return $this->redirectToRoute('my_photos');
     }
 
 }
